526937a8990d6ef06071d03d75b26369bfe4939b
rakshitraj <rraj.rakshit@gmail.com>
2020-09-23 02:09:55 +0530

    depth first search , graph

---
+++ dfs.cpp
@@ -0,0 +1,86 @@
+
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+std::vector<vector<int>> vec;
+std::vector<bool> visited;
+
+void dfs_r(int s){
+    
+    int i;
+    visited[s] = true;
+    
+    cout << "\nVisited: "<< s; 
+
+    for(i=0; i < vec[s].size(); ++i ){
+        if (visited[vec[s][i]] == false )
+            dfs_r(vec[s][i]);
+    }
+}
+
+void dfs(const std::vector<vector<int>> vec, int s){
+    int node, i;
+    std::stack<int> stk;
+    stk.push(s);
+
+    visited[s] = true;
+
+    while(!stk.empty()){
+        node = stk.top();
+        stk.pop();
+        cout << "\nVisited: "<< node;
+        for(int i=0; i < vec[node].size(); ++i)
+        {
+            if(visited[vec[node][i]] == false)
+            {
+                stk.push(vec[node][i]);
+                visited[vec[node][i]] = true;
+            }
+        }
+    }
+}
+
+void init_visited(){
+    // Initialize visited vector
+    for (int i=0; i<visited.size(); i++)
+    {
+        visited[i] = false;
+    }
+}
+
+int main()
+{
+    int nodes, edges;
+    int x[] = {1,1,2,2,3,4,4,5,5,6,6,8};
+    int y[] = {2,4,3,4,6,5,6,7,9,7,8,10};
+
+    // setting default for simplicity
+    nodes = 10;
+    edges = 12;
+
+    vec.resize(nodes+1);
+    visited.resize(nodes+1);
+
+    // Creating adjacency matrix
+    for(int i=0; i<edges; ++i)
+    {
+    
+        vec[x[i]].push_back(y[i]);
+        vec[y[i]].push_back(x[i]);
+    }
+
+    init_visited();
+
+    cout << "Recursive dfs\n";
+    dfs_r(10);
+
+    init_visited();
+
+    cout<< "\n\niterative dfs\n";
+    dfs(vec, 10);
+
+    return 0;
+}
